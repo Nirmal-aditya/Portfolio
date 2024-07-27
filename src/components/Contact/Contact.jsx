85451bfa1d4ba36cf9a50ed65042fb6d251acee8
Nirmal Aditya <140899243+Nirmal-aditya@users.noreply.github.com>
2024-07-27 19:19:48 +0530

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:nirmaladitya.personal@email.com">nirmaladitya.personal@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/nirmal-aditya-370343261/">linkedin.com/nirmal-aditya</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Nirmal-aditya">github.com/Nirmal-aditya</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
